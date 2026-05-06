58f74ee75d2194051ad388829441cb62db7290d4
genostzy <168962465+genostzy@users.noreply.github.com>
2026-05-06 12:24:35 +0800

    Delete techsprint directory

--- techsprint/src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Product, Review } from './types';
-
-export const DEFAULT_PRODUCT_IMAGE = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIiB2aWV3Qm94PSIwIDAgMTAwIDEwMCI+PHJlY3Qgd2lkdGg9IjEwMCIgaGVpZ2h0PSIxMDAiIGZpbGw9IiNmM2Y0ZjYiIC8+PC9zdmc+";
-
-export const MOCK_PRODUCTS: Product[] = [];
-
-export const MOCK_REVIEWS: Review[] = [];
